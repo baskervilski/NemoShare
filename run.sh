4ec59e44f2ea1a078571e49840ca0de5edf752ce
Nemanja Radojković <radojkovic.n@gmail.com>
2020-05-15 16:58:44 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,3 @@
-gunicorn --bind 0.0.0.0:5057 nemo_share:app --daemon
+gunicorn --bind 0.0.0.0:5057 nemo_share:app \
+    --daemon \ # for running in the background
+    --reload   # for automatic reload on detected code changes
